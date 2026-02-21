fa3580afc6bde992656455921f3e00e73080ff72
anuraj-ghawate <ghawateanuraj@gmail.com>
2026-02-22 00:29:08 +0530

    Add files via upload

---
+++ Firstt.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+//This File Is Answer Of Que 8 From Assignment 8
+int main()
+{
+    int no1 = 11, no2 = 10;
+    int ans = 0;
+    ans = Addition(no1,no2);
+    printf("Addition Is : %d\n",ans);
+    return 0;
+}
---
+++ Second.c
@@ -0,0 +1,7 @@
+//This File Is Answer Of Que 8 From Assignment 8
+int Addition (int no1,int no2)
+{
+    int result = no1 + no2;
+    
+    return result;
+}
